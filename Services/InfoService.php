275dd97cdf62088d73687fbf93f8c7e7f76c6783
Your Name <you@example.com>
2020-07-30 15:59:45 -0300

    .

---
+++ Services/InfoService.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Modules\AppendShippingCompany\Services; 
+
+
+use Modules\Portal\Services\Validation\Data\InfoValidationService;
+
+
+class InfoService extends InfoValidationService
+{
+
+
+	public function rule($data, $index, $columns)
+	{
+		return [
+'shipping_company_id' =>  'integer|min:1',
+			'shipping_company_name' =>  'string|max:255'
+		];
+	}
+
+	public function modifiers()
+	{
+		return [];
+	}
+
+
+	public function columnsFormat()
+	{
+		return  [];
+	}
+
+}
