902b3b3c55f4093d112b3ddca164a26f1c34731e
Your Name <you@example.com>
2020-10-15 13:33:59 -0300

    .

--- Services/InfoService.php
+++ Services/InfoService.php
@@ -13,8 +13,8 @@ class InfoService extends InfoValidationService
 	public function rule($data, $index, $columns)
 	{
 		return [
-'shipping_company_id' =>  'integer|min:1',
-			'shipping_company_name' =>  'string|max:255'
+			'shipping_company_id' =>  'nullable|integer|min:1',
+			'shipping_company_name' =>  'nullable|string|max:255'
 		];
 	}
 
